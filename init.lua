9ce50f751ad37dc696397aba082c702a28a56c83
tomlommerse <tomlommerse2001@gmail.com>
2025-03-31 00:18:59 +0200

    initial commit

---
+++ init.lua
@@ -0,0 +1,2 @@
+require("remaps")
+require("plugins")
---
+++ lua/plugins/packer.lua
@@ -0,0 +1,21 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim'
+
+	use{
+		'nvim-telescope/telescope.nvim', tag = '0.1.8',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	use{ 
+		"catppuccin/nvim",
+		as = "catppuccin",
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha",
+			})
+			vim.cmd("colorscheme catppuccin")
+		end
+	}
+end)
